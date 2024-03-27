9edf259ac0e9de9f08c0abb1bba54b377811fa29
Ilia Ciolac <cholak_01@mail.ru>
2024-03-27 14:57:07 +0100

    Initial

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoUrl = process.env.MONGO_URL;
+
+const app = express();
+
+app.use(cors({ credentials: true }));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+mongoose
+  .connect(mongoUrl, {
+    serverSelectionTimeoutMS: 5000,
+  })
+  .then(() => {
+    console.log("Connected to Mongo Database");
+    app.listen(8081, () => {
+      console.log("Server started on port 8081");
+    });
+  })
+  .catch((err: Error) => {
+    console.log(err);
+  });
